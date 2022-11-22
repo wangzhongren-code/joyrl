f6f5c3c334f49432fe7188f24b61de70c1d4a116
johnjim0816 <johnjim0816@gmail.com>
2022-11-22 12:44:20 +0800

    test

--- conf.py
+++ conf.py
@@ -5,11 +5,13 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import joyrl
+import sphinx_rtd_theme
 
 project = 'JoyRL'
 copyright = '2022, johnjim0816'
 author = 'johnjim0816'
-release = '0.1.13'
+release = joyrl.__version__
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -18,11 +20,16 @@ extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+source_suffix = ['.rst']
+master_doc = 'index'
 
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
